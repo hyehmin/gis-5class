3d01414cf143b190715242d65ac71e64d95acf36
hyehmin <qqqwe10000@naver.com>
2021-07-13 15:59:22 +0900

    2021-07-13 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
@@ -24,7 +25,7 @@ def hello_world(request):
 
 
 class AccountCreateView(CreateView):
-    model = UserWarning
+    model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountsapp/create.html'
