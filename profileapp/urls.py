bc48fc10327a5fd87876910f50b79e92d6a12060
hyehmin <qqqwe10000@naver.com>
2021-07-26 10:35:21 +0900

    2021-07-26 commit

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,3 +1,7 @@
-urlpatterns = [
+from django.urls import path
 
+from profileapp.views import ProfileCreateView
+
+urlpatterns = [
+    path('create/', ProfileCreateView.as_view(), name='create')
 ]
