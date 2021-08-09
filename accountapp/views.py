1399952c72d85f027b8932a4042e63512b024e96
hyehmin <qqqwe10000@naver.com>
2021-08-09 10:35:53 +0900

    2021-08-08 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,65 +1,62 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
+from django.views.generic.edit import FormMixin
 
-from accountapp.decorator import account_ownership_required
-from accountapp.forms import AccountCreationForm
-from accountapp.models import HelloWorld
-
-
-@login_required
-def hello_world(request):
-    if request.method == "POST":
-        temp = request.POST.get('hello_world_input')
-
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list'})
+from articleapp.forms import ArticleCreationForm
+from articleapp.models import Article
+from articleapp.templates.decorator import article_ownership_required
+from commentapp.forms import CommentCreationForm
 
 
-class AccountCreateView(CreateView):
-    model = User
-    form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
-    template_name = 'accountapp/create.html'
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreationForm
+    success_url = reverse_lazy('articleapp:list')
+    template_name = 'articleapp/create.html'
 
-
-class AccountDetailView(DetailView):
-    model = User
-    context_object_name = 'target_user'
-    template_name = 'accountapp/detail.html'
-
-has_ownership = [login_required, account_ownership_required]
-
-@method_decorator(has_ownership,'get')
-@method_decorator(has_ownership,'post')
-class AccountUpdateView(UpdateView):
-    model = User
-    form_class = AccountCreationForm
-    context_object_name = 'target_user'
-    template_name = 'accountapp/update.html'
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
+        return reverse('articleapp:detail', kwargs={'pk': self.object})
+
+class ArticleDetailView(DetailView, FormMixin):
+    model = Article
+    form_class = CommentCreationForm
+    context_object_name = 'target_article'
+    template_name = 'articleapp/detail.html'
 
 
-@method_decorator(has_ownership,'get')
-@method_decorator(has_ownership,'post')
-class AccountDeleteView(DeleteView):
-    model = User
-    context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
-    template_name = 'accountapp/delete.html'
+@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleCreationForm
+    context_object_name = 'target_article'
+    success_url = reverse_lazy('articleapp:list')
 
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
+
+
+@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
+class ArticleDeleteView(DeleteView):
+    model = Article
+    context_object_name = 'target_article'
+    success_url = reverse_lazy('articleapp:list')
+    template_name = 'articleapp:delete.html'
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'articleapp/list.html'
+    paginate_by = 20
